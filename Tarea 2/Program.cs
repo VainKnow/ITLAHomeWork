ac87f0c6394cd1e173a8af0066d4a53559bf2dc9
Jesus <jesus15matos28@gmail.com>
2025-10-02 17:20:29 -0400

    UPDATING TAREA 2

---
+++ Tarea 2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ITLA
+
+//Jesus Matos Matricula:20241767
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool WhileEstructure = true;
+
+            while (WhileEstructure)
+            {
+
+
+                try
+                {
+                    DateTime dateTime = DateTime.Now;
+                    Console.WriteLine(dateTime);
+
+                    int Number1;
+                    int value;
+
+                    Console.WriteLine("write the number");
+                    Number1 = int.Parse(Console.ReadLine());
+
+                    value = Number1 % 2;
+
+                    if (value == 0)
+                    {
+
+                        Console.WriteLine(" your number is even");
+                    }
+                    else
+                    {
+                        Console.WriteLine("your number is odd");
+
+                    }
+
+                    Console.WriteLine("Do you want to run the code again? (s/n)");
+                    string response = Console.ReadLine().ToLower();
+                    if (response != "s")
+                    {
+                        WhileEstructure = false;
+                    }
+                    Console.Clear();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error inesperado: {ex.Message}");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
