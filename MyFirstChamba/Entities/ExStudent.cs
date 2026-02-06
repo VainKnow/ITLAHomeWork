0dcddc167706382a7de0090fc730b2afab85e560
VainKnow <130000740+VainKnow@users.noreply.github.com>
2026-02-06 19:09:57 -0400

    Tarea 1/P2

---
+++ MyFirstChamba/Entities/ExStudent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstChamba.Entities
+{
+    internal class ExStudent : Student
+    {
+        public string Reason { get; set; }
+
+        public DateTime ExclusionDate { get; set; }
+
+        public bool Graduated { get; set; }
+    }
+}
---
+++ MyFirstChamba/Entities/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstChamba.Entities
+{
+    internal class Student: CommunityMember
+    {
+        public string Carrier { get; set; }
+
+        public int Semester { get; set; }
+
+
+
+
+    }
+}
