6d28db86fc60874b0e9867658f6f93cde4dbb2ce
VainKnow <130000740+VainKnow@users.noreply.github.com>
2026-02-06 19:04:15 -0400

    Primera tarea de programacion 2

---
+++ Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstChamba.Entities
+{
+    internal class Employee: CommunityMember
+    {
+        public decimal Salary { get; set; }
+
+        public int SNS  { get; set; }
+
+        public String Job { get; set; }
+
+
+    }
+}
