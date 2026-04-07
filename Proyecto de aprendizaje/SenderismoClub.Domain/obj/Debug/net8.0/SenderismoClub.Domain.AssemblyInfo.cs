850129b332069461f45db8b831dc2a12e7c93149
Jesus <jesus15matos28@gmail.com>
2026-04-07 17:05:11 -0400

    Trabajo 2

--- Proyecto de aprendizaje/SenderismoClub.Domain/obj/Debug/net8.0/SenderismoClub.Domain.AssemblyInfo.cs
+++ Proyecto de aprendizaje/SenderismoClub.Domain/obj/Debug/net8.0/SenderismoClub.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SenderismoClub.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f987b40cebde2c410717f0118162c4de829bd046")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b6bf749de282b0f02915fe978781d86a3483f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SenderismoClub.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SenderismoClub.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
